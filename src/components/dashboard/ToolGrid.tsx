1124b9f16ecaffd6661e83228a65f67b068d11ac
akshay-1a <akshay1.py@gmail.com>
2025-01-31 01:03:14 +0530

    update toolgrid

--- src/components/dashboard/ToolGrid.tsx
+++ src/components/dashboard/ToolGrid.tsx
@@ -22,7 +22,7 @@ export function ToolGrid({ tools, activePlatform, onToolLaunch }: ToolGridProps)
         <MagicCard
           key={index}
           className={cn(
-            "cursor-pointer shadow-2xl group relative overflow-hidden bg-primary/50  border-white/50 flex flex-col",
+            "cursor-pointer shadow-2xl shadow-primary/10 group relative overflow-hidden bg-primary/50  border-white/50 flex flex-col",
             tool.comingSoon && "opacity-80"
           )}
           gradientColor={theme === "dark" ? "#000" : "#D9D9D9"}
@@ -33,9 +33,9 @@ export function ToolGrid({ tools, activePlatform, onToolLaunch }: ToolGridProps)
             </div>
           )} */}
 
-          <div className="flex-1 flex gap-5 p-4 md:p-6">
+          <div className="flex-1 grid grid-cols-2 gap-5 p-4 md:p-6">
             <div className="rounded-lg group-hover:rounded-3xl w-40 h-full flex bg-zinc-100 p-2 md:p-3 flex-shrink-0 transition-all duration-500 ease-in-out">
-              <tool.icon className="size-full p-3 group-hover:p-6 text-primary/70 transition-all duration-300 ease-in-out " />
+              <tool.icon className="size-full p-6 group-hover:p-3 text-primary/70 transition-all duration-300 ease-in-out " />
             </div>
             <div className="flex flex-col items-start gap-3 md:gap-4">
               <div className="min-w-0 flex-1">
@@ -57,7 +57,7 @@ export function ToolGrid({ tools, activePlatform, onToolLaunch }: ToolGridProps)
                             p === 'linkedin' && "bg-blue-100/50 dark:bg-blue-950/50"
                           )}
                         >
-                          <config.icon className={`h-6 w-6 ${config.color}`} />
+                          <config.icon className={`size-5 ${config.color}`} />
                         </div>
                       );
                     })}
