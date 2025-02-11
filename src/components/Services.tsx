ca1f1a84a41c4033210fb93cd4d512424f62b70a
Arjun Bhandari <arjun12345bhandari@gmail.com>
2025-02-12 00:02:41 +0530

    update services image

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -11,14 +11,14 @@ export function Services() {
           </p>
           <div className="grid grid-cols-2 gap-4">
             <img
-              src="https://res.cloudinary.com/dvuohzc5b/image/upload/v1738858670/b1rvszglsbaq0p3ax1ei.png"
+              src="https://res.cloudinary.com/dvuohzc5b/image/upload/v1739286646/Screenshot_2025-02-11_at_8.39.01_PM_re1lrg.png"
               alt="startup template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <img
-              src="https://res.cloudinary.com/dvuohzc5b/image/upload/v1738858669/ttuucrk3khby891ayw8r.png"
+              src="https://res.cloudinary.com/dvuohzc5b/image/upload/v1739286671/Screenshot_2025-02-11_at_8.39.14_PM_bh5zfu.png"
               alt="startup template"
               width={500}
               height={500}
